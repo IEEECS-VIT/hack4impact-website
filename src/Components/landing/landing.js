48da26807ee2436bda113706f6b6551c6db7992d
ANIKET CHAWLA <aniket.chawla03@gmail.com>
2023-06-03 23:54:32 +0530

    faq resonsive

--- src/Components/landing/landing.js
+++ src/Components/landing/landing.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import './landing.css';
-import logo from './Shape.svg';
-import Countdown from '../countdown/countdown';
+import React from "react";
+import "./landing.css";
+import logo from "./Shape.svg";
+import Countdown from "../countdown/countdown";
 
 const landing = () => {
-  const time = new Date('June 6, 2023 13:00:00').getTime();
+  const time = new Date("June 6, 2023 13:00:00").getTime();
   const HandleRedirect = () => {
-    window.open('https://vtop.vit.ac.in/vtop/login', '_blank');
+    window.open("https://vtop.vit.ac.in/vtop/login", "_blank");
   };
 
   return (
-    <div>
-      <div class='titlenav'>
-        <div class='titleclass'>
-          <h1 class='title'>
-            HACK 4 IMPACT <span class='titleYear'>2023</span>
+    <div class="landing">
+      <div class="titlenav">
+        <div class="titleclass">
+          <h1 class="title">
+            HACK 4 IMPACT <span class="titleYear">2023</span>
           </h1>
         </div>
 
-        <nav id='navbar' class='navigation' role='navigation'>
-          <input id='toggle1' type='checkbox' style={{ display: 'none' }} />
-          <label class='hamburger1' for='toggle1'>
-            <div class='top'></div>
-            <div class='meat'></div>
-            <div class='bottom'></div>
+        <nav id="navbar" class="navigation" role="navigation">
+          <input id="toggle1" type="checkbox" style={{ display: "none" }} />
+          <label class="hamburger1" for="toggle1">
+            <div class="top"></div>
+            <div class="meat"></div>
+            <div class="bottom"></div>
           </label>
 
-          <nav class='menu1'>
-            <a class='link1' href='#about'>
+          <nav class="menu1">
+            <a class="link1" href="#about">
               ABOUT
             </a>
-            <a class='link1' href='#sponsors'>
+            <a class="link1" href="#sponsors">
               SPONSORS
             </a>
-            <a class='link1' href='#organizers'>
+            <a class="link1" href="#organizers">
               ORGANIZERS
             </a>
-            <a class='link1' id='faq' href='#faq'>
+            <a class="link1" id="faq" href="#faq">
               FAQ
             </a>
           </nav>
         </nav>
       </div>
 
-      <div class='logodiv'>
-        <img class='logo' src={logo} alt='shape' />
+      <div class="logodiv">
+        <img class="logo" src={logo} alt="shape" />
       </div>
-      <div class='parenttxt'>
-        <p class='empowertxt'>Empowering Minds & Igniting Changes</p>
+      <div class="parenttxt">
+        <p class="empowertxt">Empowering Minds & Igniting Changes</p>
       </div>
       <div>
         <Countdown targetDate={time} />
       </div>
-      <div class='btndiv'>
-        <button class='btn' type='button' onClick={HandleRedirect}>
+      <div class="btndiv">
+        <button class="btn" type="button" onClick={HandleRedirect}>
           Register Now
         </button>
       </div>
