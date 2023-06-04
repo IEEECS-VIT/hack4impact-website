918c13b8f43f6bc48301657857b59b03a9322eef
ANIKET CHAWLA <aniket.chawla03@gmail.com>
2023-06-04 20:40:02 +0530

    feat: timeline

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import Landing from './Components/landing/landing.js';
-import Orgteam from './Components/orgteam/orgteam.js';
-import { Element } from 'react-scroll';
-import './App.css';
+import React from "react";
+import Landing from "./Components/landing/landing.js";
+import Orgteam from "./Components/orgteam/orgteam.js";
+import { Element } from "react-scroll";
+import "./App.css";
 
-import Abouthack from './Components/about/about-hack.js';
-import AboutVIT from './Components/about/about-vit.js';
-import AboutYantra from './Components/about/about-yantra.js';
-import Content from './Components/faq-content/content.js';
-import Sponsors from './Components/sponsors/sponsors.js';
-import './App.css';
+import Abouthack from "./Components/about/about-hack.js";
+import AboutVIT from "./Components/about/about-vit.js";
+import AboutYantra from "./Components/about/about-yantra.js";
+import Content from "./Components/faq-content/content.js";
+import Sponsors from "./Components/sponsors/sponsors.js";
+import Timline from "./Components/timeline/timeline.js";
+
+import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Landing />
-      <div style={{ marginTop: '15rem' }}>
+      <div style={{ marginTop: "15rem" }}>
         <AboutYantra />
         <Abouthack />
       </div>
-      <div style={{ marginTop: '15rem' }}>
+      <div style={{ marginTop: "15rem" }}>
         <Orgteam />
       </div>
       <Content />
+      <Timline />
       <Sponsors />
     </div>
   );
---
+++ src/Components/timeline/timeline.js
@@ -0,0 +1,128 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import "./timeline.css";
+
+const timeline = () => {
+  return (
+    <div>
+      <div class="heading">Timeline</div>
+      <hr class="line-time" />
+      <VerticalTimeline>
+        <h2 className="date-time">
+          June 6 <span class="day-color">Day 1</span>
+        </h2>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="9 AM-10 AM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Event Registration
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="10 AM - 3 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Work On Project</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="3 PM - 4 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Speaker Session -1
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="6:30 PM - 7 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Comedy Club Session
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="10 PM - 11 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Review 1</h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+
+      <VerticalTimeline>
+        <h2 className="date-time">
+          June 7 <span class="day-color">Day 2</span>
+        </h2>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="9 AM - 10 AM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Review 2</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="10 PM - 11 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Speaker Session 2</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="11 AM - 12:30 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Submission For{" "}
+            <main class=".vertical-timeline-element-title">
+              Midhack Evaluation
+            </main>
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="3:30 PM - 5:10 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Final Pitching</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="5:10 PM - 5:30 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Evaluation</h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="5:30 PM - 6 PM"
+          iconStyle={{ background: "#fff", color: "#fff" }}
+          //   icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Result And Prices</h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+};
+
+export default timeline;
